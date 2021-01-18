92e5e7fc1c687f262ad81c0a99c2ed61600462d7
JaniNiemi <janitniemi@gmail.com>
2021-01-18 18:22:58 +0200

    add completion state toggle on click

--- app.js
+++ app.js
@@ -15,21 +15,24 @@ function buildDOM() {
     tasksEl.innerHTML = "";
     tasks.forEach((task, i) => {
         const taskEl = document.createElement("li");
-        taskEl.innerHTML = `<p>${task.task}</p> <button class="remove-task" data-index="${i}" onclick="removeTask(${i})"><i class="fa fa-times"></i>`;
+        taskEl.innerHTML = `<p onclick="taskCompleted(${i})">${task.task}</p> <button class="remove-task" data-index="${i}" onclick="removeTask(${i})"><i class="fa fa-times"></i>`;
         taskEl.classList.add("task");
+        if(task.completed) {
+            taskEl.classList.add("completed");
+        }
         tasksEl.appendChild(taskEl);
     });
     handleLocalStorage();
 }
 
-// add new task when form is submitted
+// add new task when form is submitted, update DOM
 function addTask(e) {
     e.preventDefault();
     if(newTaskInput.value.trim() === "") {
         alert("Please write a task");
         return false;
     }
-    tasks.push({task: newTaskInput.value},);
+    tasks.push({task: newTaskInput.value, completed: false},);
     newTaskForm.reset();
     buildDOM();
 }
@@ -41,6 +44,17 @@ function removeTask(index) {
     setTimeout(() => {
         buildDOM();
     }, 500);
+}
+
+// set task completion state on click
+function taskCompleted(index) {
+    tasks[index].completed = !tasks[index].completed
+    if(tasks[index].completed) {
+        document.querySelectorAll(".task")[index].classList.add("completed");
+    } else {
+        document.querySelectorAll(".task")[index].classList.remove("completed");
+    }
+    handleLocalStorage();
 }
 
 // event listeners
